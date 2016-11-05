6de8a1ae87910a5ebbde34b3fbe04fc76a5e4a10
lx4r <hallo@l3r.de>
2016-11-05 15:05:13 +0100

    First (not yet working) version of a color picker in the projects window

--- app/vue/projects-window/project-input.vue
+++ app/vue/projects-window/project-input.vue
@@ -6,12 +6,31 @@
                     <h3 class="panel-title">Add project</h3>
                 </div>
                 <div class="panel-body">
-                    <form>
-                        <div class="form-group">
+                    <div class="form-group">
+                        <div class="input-group">
                             <input type="text" class="form-control" id="projectName" placeholder="project name" v-model="projectName">
+                            <div class="input-group-btn">
+                                <div class="dropdown">
+                                    <button id="dLabel" type="button" class="btn btn-default" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Color
+                                        <span class="caret"></span>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dLabel">
+                                        <div class="color-picker-row">
+                                            <div class="color-example color-example-selected" id="ce1">&nbsp;</div>
+                                            <div class="color-example" id="ce2">&nbsp;</div>
+                                            <div class="color-example" id="ce3">&nbsp;</div>
+                                            <div class="color-example color-example-end" id="ce4">&nbsp;</div>
+                                        </div>
+                                        <div class="color-picker-row">
+                                            <div class="color-example" id="ce5">&nbsp;</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <button class="btn btn-success" id="addProjectButton" type="button" v-on:click="addProject(projectName)">Add project</button>
-                    </form>
+                    </div>
+                    <button class="btn btn-success" id="addProjectButton" type="button" v-on:click="addProject(projectName)">Add project</button>
                 </div>
             </div>
         </div>
