3a8ef1860e11a0a14d3a0acdc24fa1e1f704be1f
lx4r <hallo@l3r.de>
2017-03-27 13:13:43 +0200

    small fix for the project list

--- app/vue/main-window/projects-dropdown.vue
+++ app/vue/main-window/projects-dropdown.vue
@@ -1,6 +1,6 @@
 <template>
     <select class="form-control" id="projectsDropdown">
-        <option v-for="project in pl" v-bind:value="project[0]">
+        <option v-for="project in pl" :value="project[0]">
             {{project[1].name}}
         </option>
     </select>
