eeb2b48d062a0f5016e7126bbe49dffa3aea40d3
lx4r <hallo@l3r.de>
2017-04-03 13:58:42 +0200

    removing table, adding first version of new layout

--- app/vue/main-window/activity-list.vue
+++ app/vue/main-window/activity-list.vue
@@ -6,38 +6,30 @@
                     <h3 class="panel-title">Activities</h3>
                 </div>
                 <div v-if="dataReceived" id="activity-list-table">
-                    <table class="table table-striped" v-if="activities.activitiesArray.length > 0">
-                        <thead>
-                        <tr>
-                            <th>Activity</th>
-                            <th>Project</th>
-                            <th>Time</th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr v-for="activity in activities.activitiesArray">
-                            <td>{{activity[1].name}}</td>
-                            <td>
-                                <span class="badge" :style='"background-color:" + getProjectColor(pl, activity[1].projectID)'>{{getProjectName(pl, activity[1].projectID)}}</span>
-                            </td>
-                            <td>
-                                <a type="button" v-bind:href="'#activity-' + activity[0]" data-toggle="collapse" v-bind:data-target="'#activity-' + activity[0]">{{secondsToTime(activity[1].duration)}}</a>
-                                <div class="collapse" v-bind:id="'activity-' + activity[0]">
-                                    {{timestampToDate(activity[1].startTime)}} - {{timestampToDate(activity[1].endTime)}}
+                    <ul class="list-group" v-if="activities.activitiesArray.length > 0">
+                        <li class="list-group-item" v-for="activity in activities.activitiesArray">
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    {{activity[1].name}}<br>
+                                    <span class="badge" :style='"background-color:" + getProjectColor(pl, activity[1].projectID)'>{{getProjectName(pl, activity[1].projectID)}}</span><br>
                                 </div>
-                            </td>
-                            <td>
-                                <button v-on:click="deleteActivity(activity[0])" type="button" class="btn btn-xs btn-danger deleteActivityButton" aria-label="Left Align">
-                                    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                                </button>
-                                <button type="button" class="btn btn-xs btn-info" data-toggle="modal" data-target="#edit-activity" v-on:click="setActivityToEdit(activity)">
-                                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                                </button>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                                <div class="col-xs-2">
+                                    <button v-on:click="deleteActivity(activity[0])" type="button" class="btn btn-xs btn-danger deleteActivityButton" aria-label="Left Align">
+                                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                                    </button>
+                                    <button type="button" class="btn btn-xs btn-info" data-toggle="modal" data-target="#edit-activity" v-on:click="setActivityToEdit(activity)">
+                                        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                                    </button>
+                                </div>
+                                <div class="col-xs-4">
+                                    <a type="button" v-bind:href="'#activity-' + activity[0]" data-toggle="collapse" v-bind:data-target="'#activity-' + activity[0]">{{secondsToTime(activity[1].duration)}}</a>
+                                    <div class="collapse" v-bind:id="'activity-' + activity[0]">
+                                        {{timestampToDate(activity[1].startTime)}} - {{timestampToDate(activity[1].endTime)}}
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                     <div class="text-muted" v-else>
                         No activities yet.
                     </div>
